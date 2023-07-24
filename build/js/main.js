7f61b2665ccb8e5bbde5eaa3772808be056f4bde
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-07-24 22:43:19 +0200

    Objects,Arrays,Tuples and Enums

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,36 @@
 "use strict";
-let myName = "Dave";
-let meaningOfLife;
-let isLoading;
-let album;
-myName = "John";
-meaningOfLife = 42;
-isLoading = true;
-album = 'Van Halen';
-const sum = (a, b) => {
-    return a + b;
+let stringArr = ['one', 'hey', 'Dave'];
+let guitars = ['Strat', 'Les Paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'John';
+stringArr.push('hey');
+guitars[0] = 1984;
+guitars.unshift('Jim');
+let test = [];
+let bands = [];
+bands.push('Vna Helen');
+// Tuple
+let myTuple = ['Deve', 42, true];
+let mixed = ['John', 1, false];
+myTuple[1] = 42;
+//objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
 };
-let postId;
-let isActive;
-let re = /\w+/g;
+exampleObj.prop1 = 'John';
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812'],
+};
+let jp = {
+    name: 'Jimmy',
+    albums: ['I', 'II', 'IV'],
+};
+const greetGuitarist = (guitarist) => ;
